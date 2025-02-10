aef032a59d1afe5c9d24ce096c466a4533dd0b42
jacob <jaweiss2305@gmail.com>
2025-02-10 10:27:09 -0700

    add examples

---
+++ docs/examples/agent/simple_tool_call.py
@@ -0,0 +1,36 @@
+# pip install pixeltable openai yfinance
+
+from typing import Optional
+
+import pixeltable as pxt
+import yfinance as yf
+
+import openai_agent
+
+
+@pxt.udf
+def stock_info(ticker: str) -> Optional[dict]:
+    """Get stock info for a given ticker symbol."""
+    stock = yf.Ticker(ticker)
+    return stock.info
+
+
+# Persistent Agent with memory and tools
+openai_agent.init(
+    agent_name="Financial_analyst",
+    system_prompt="You are a financial analyst, who can access yahoo finance data. Help the user with their stock analysis.",
+    model_name="gpt-4o-mini",
+    agent_tools=pxt.tools(stock_info),
+    reset_memory=True,  # set to true to delete the agent and start fresh
+)
+
+report = openai_agent.run(
+    agent_name="Financial_analyst",
+    message="""
+    Fetch the latest stock information for Factset (Ticker: FDS). 
+    Then create a 100 summary of the company as a financial report in markdown format.
+    """,
+)
+
+# inspect
+print(report)
