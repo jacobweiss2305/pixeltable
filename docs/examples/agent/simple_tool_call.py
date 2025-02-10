e499ed7ab0fb231382f39272e32908ddae88d315
jacob <jaweiss2305@gmail.com>
2025-02-10 10:27:46 -0700

    add formatter

--- docs/examples/agent/simple_tool_call.py
+++ docs/examples/agent/simple_tool_call.py
@@ -17,15 +17,15 @@ def stock_info(ticker: str) -> Optional[dict]:
 
 # Persistent Agent with memory and tools
 openai_agent.init(
-    agent_name="Financial_analyst",
-    system_prompt="You are a financial analyst, who can access yahoo finance data. Help the user with their stock analysis.",
-    model_name="gpt-4o-mini",
+    agent_name='Financial_analyst',
+    system_prompt='You are a financial analyst, who can access yahoo finance data. Help the user with their stock analysis.',
+    model_name='gpt-4o-mini',
     agent_tools=pxt.tools(stock_info),
     reset_memory=True,  # set to true to delete the agent and start fresh
 )
 
 report = openai_agent.run(
-    agent_name="Financial_analyst",
+    agent_name='Financial_analyst',
     message="""
     Fetch the latest stock information for Factset (Ticker: FDS). 
     Then create a 100 summary of the company as a financial report in markdown format.
